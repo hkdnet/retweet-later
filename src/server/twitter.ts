5f8d360ded49d22c3dc1bf1b6cbb360ea8640353
hkdnet <satoko.itse@gmail.com>
2018-01-21 23:50:12 +0900

    Add a class Twitter

---
+++ src/server/twitter.ts
@@ -0,0 +1,17 @@
+import * as Twit from 'twit'
+
+export default class Twitter {
+  private T: Twit;
+
+  constructor(params: Twit.Options) {
+    this.T = new Twit(params);
+  }
+
+  search(params?: Twit.Params) {
+    return this.T.get('search/tweets', params)
+  }
+
+  retweet(id: string, callback: Twit.Callback) {
+    return this.T.post('statuses/retweet', {id}, callback)
+  }
+}
